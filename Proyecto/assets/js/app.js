51f4ec42b765989b2278a41fda838a3db9b987e6
dcasalv023 <dcasalv023@educaand.es>
2024-12-08 20:39:01 +0100

    Responsive

--- Proyecto/assets/js/app.js
+++ Proyecto/assets/js/app.js
@@ -3,3 +3,10 @@ import 'bootstrap';
 
 // Importa el archivo principal de estilos
 import '../styles/app.scss';
+
+// assets/js/app.js
+import './price-range.js';
+
+// assets/js/app.js
+import './DetallesProducto.js';
+
